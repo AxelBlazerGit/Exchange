38c54607a6625597c449f4470f80c7b8f61bf65a
sounava24 <sounavachatterjee32@gmail.com>
2024-08-24 22:26:28 +0530

    fifth DRAFT

--- frontend/src/pages/LoginForm.jsx
+++ frontend/src/pages/LoginForm.jsx
@@ -1,26 +1,25 @@
-import React,{useState} from 'react'
-import {Facebook,GitHub,Google} from '@mui/icons-material'
+import React, { useState } from 'react';
+import { Facebook, GitHub, Google } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
 
-  const [isLogin,setIsLogin] = useState(true);
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
 
-  const [loginEmail,setLoginEmail] = useState("");
-  const [loginPassword,setLoginPassword] = useState("");
-
-  const [registerEmail,setRegisterEmail] = useState("");
-  const [registerPassword,setRegisterPassword] = useState("");
-  const [registerName,setRegisterName] = useState("");
-  const [registerAvatar,setRegisterAvatar] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerName, setRegisterName] = useState("");
+  const [registerAvatar, setRegisterAvatar] = useState("");
 
   const handleLogin = () => {
     const userLoginData = {
       email: loginEmail,
+      password: loginPassword,
     };
     console.log(userLoginData);
-
-  }
+  };
 
   return (
     <div className="bg-teal-50 flex flex-col items-center justify-center min-h-screen md:py-2">
@@ -31,41 +30,57 @@ const LoginForm = () => {
             Swap, Share, and Learn: Connecting Students Through Books and Gear Across Years.
           </p>
         </div>
-        {/* past start here */}
 
+        {/* Login Form */}
         <div className="bg-teal-100 rounded-2xl shadow-2xl flex flex-col w-full md:w-1/3 items-center max-w-4xl transition duration-1000 ease-out p-6">
-        <Link className="p-3 text-3xl font-bold text-teal-500" to="/">UniHub</Link>
-        <div className="inline-block border-[2px] justify-center w-20 border-teal-400 border-solid mb-2"></div>
-        <h3 className="text-xl font-semibold text-teal-500 pt-2">Sign In!</h3>
+          <Link className="p-3 text-3xl font-bold text-teal-500" to="/">UniHub</Link>
+          <div className="inline-block border-[2px] justify-center w-20 border-teal-400 border-solid mb-2"></div>
+          <h3 className="text-xl font-semibold text-teal-500 pt-2">Sign In!</h3>
 
-        <div className="flex space-x-4 mt-4 items-center justify-center">
-          <Facebook className="text-teal-500 hover:text-teal-700 cursor-pointer" />
-          <GitHub className="text-teal-500 hover:text-teal-700 cursor-pointer" />
-          <Google className="text-teal-500 hover:text-teal-700 cursor-pointer" />
+          <div className="flex space-x-4 mt-4 items-center justify-center">
+            <Facebook className="text-teal-500 hover:text-teal-700 cursor-pointer" />
+            <GitHub className="text-teal-500 hover:text-teal-700 cursor-pointer" />
+            <Google className="text-teal-500 hover:text-teal-700 cursor-pointer" />
+          </div>
+
+          <div className="flex flex-col items-center justify-center mt-4">
+            <input
+              id="loginEmail"
+              name="loginEmail"
+              type="email"
+              className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-slate-700 focus:outline-none transition duration-300"
+              placeholder="Email"
+              value={loginEmail}
+              onChange={(e) => setLoginEmail(e.target.value)}
+            />
+            <input
+              id="loginPassword"
+              name="loginPassword"
+              type="password"
+              className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-slate-700 focus:outline-none transition duration-300"
+              placeholder="Password"
+              value={loginPassword}
+              onChange={(e) => setLoginPassword(e.target.value)}
+            />
+            <button
+              onClick={handleLogin}
+              className="rounded-2xl m-2 text-white bg-teal-500 w-3/5 px-4 py-2 shadow-md hover:text-teal-500 hover:bg-white border-[2px] border-teal-500 transition duration-300"
+            >
+              Sign In
+            </button>
+          </div>
+
+          <p className="text-teal-500 m-4 text-sm">
+            Don't have an account?
+            <Link
+              to="/sign"
+              className="underline underline-offset-4 text-teal-600 ml-1 cursor-pointer hover:text-slate-700 transition duration-300"
+              onClick={() => setIsLogin(false)}
+            >
+              Sign Up
+            </Link>
+          </p>
         </div>
-
-        <div className="flex flex-col items-center justify-center mt-4">
-          <input 
-          id="email"
-          name='email'
-          type="email" 
-          className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-slate-700 focus:outline-none transition duration-300" 
-          placeholder="Email" 
-          value={loginEmail}
-          onChange={(e) => setLoginEmail(e.target.value)}/>
-          <input type="password" className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-slate-700 focus:outline-none transition duration-300" placeholder="Password" />
-          <button 
-          onClick={handleLogin}
-          className="rounded-2xl m-2 text-white bg-teal-500 w-3/5 px-4 py-2 shadow-md hover:text-teal-500 hover:bg-white border-[2px] border-teal-500 transition duration-300">
-            Sign In
-          </button>
-        </div>
-
-        <p className="text-teal-500 m-4 text-sm">
-          Don't have an account?
-          <Link to='/sign' className="underline underline-offset-4 text-teal-600 ml-1 cursor-pointer hover:text-slate-700 transition duration-300" onClick={() => setIsLogin(false)}>Sign Up</Link>
-        </p>
-      </div>
       </main>
     </div>
   );
--- frontend/src/pages/SignUpForm.jsx
+++ frontend/src/pages/SignUpForm.jsx
@@ -1,27 +1,35 @@
-import React,{useState} from 'react'
-import {Facebook,GitHub,Google} from '@mui/icons-material'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Facebook, GitHub, Google } from '@mui/icons-material';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUpForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
 
-  const [isLogin,setIsLogin] = useState(true);
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerName, setRegisterName] = useState("");
+  const [registerInstitution, setRegisterInstitution] = useState("");
+  const [registerPlace, setRegisterPlace] = useState("");
 
-  const [loginEmail,setLoginEmail] = useState("");
-  const [loginPassword,setLoginPassword] = useState("");
+  const navigate = useNavigate(); // For redirection
 
-  const [registerEmail,setRegisterEmail] = useState("");
-  const [registerPassword,setRegisterPassword] = useState("");
-  const [registerName,setRegisterName] = useState("");
-  const [registerAvatar,setRegisterAvatar] = useState("");
-
-  const handleLogin = () => {
-    const userLoginData = {
-      email: loginEmail,
+  const handleSignUp = () => {
+    const userRegistrationData = {
+      name: registerName,
+      email: registerEmail,
+      password: registerPassword,
+      institution: registerInstitution,
+      place: registerPlace,
     };
-    console.log(userLoginData);
+    console.log(userRegistrationData);
 
-  }
-  
+    // Here you would typically send the data to the backend for signup
+    // After signup is successful, redirect to homepage
+    // Simulating signup completion with a timeout for example purpose
+    setTimeout(() => {
+      navigate("/"); // Redirect to home page after successful signup
+    }, 1000); // Simulating a delay for signup completion
+  };
 
   return (
     <div className="bg-teal-50 flex flex-col items-center justify-center min-h-screen md:py-2">
@@ -32,41 +40,84 @@ const SignUpForm = () => {
             Swap, Share, and Learn: Connecting Students Through Books and Gear Across Years.
           </p>
         </div>
-        {/* past start here */}
-      <div className="bg-slate-700 text-white rounded-2xl shadow-2xl flex flex-col w-full md:w-1/3 items-center max-w-4xl transition duration-1000 ease-in p-6">
-        
-        <Link className="p-3 text-3xl font-bold text-white" to="/">UniHub</Link>
-        <div className="inline-block border-[2px] justify-center w-20 border-white mb-2"></div>
-        <h3 className="text-xl font-semibold text-white pt-2">Create Account!</h3>
 
-        <div className="flex space-x-4 mt-4 items-center justify-center">
-          <Facebook className="text-white hover:text-teal-300 cursor-pointer" />
-          <GitHub className="text-white hover:text-teal-300 cursor-pointer" />
-          <Google className="text-white hover:text-teal-300 cursor-pointer" />
+        {/* SignUp Form */}
+        <div className="bg-slate-700 text-white rounded-2xl shadow-2xl flex flex-col w-full md:w-1/3 items-center max-w-4xl transition duration-1000 ease-in p-6">
+          <Link className="p-3 text-3xl font-bold text-white" to="/">UniHub</Link>
+          <div className="inline-block border-[2px] justify-center w-20 border-white mb-2"></div>
+          <h3 className="text-xl font-semibold text-white pt-2">Create Account!</h3>
+
+          <div className="flex space-x-4 mt-4 items-center justify-center">
+            <Facebook className="text-white hover:text-teal-300 cursor-pointer" />
+            <GitHub className="text-white hover:text-teal-300 cursor-pointer" />
+            <Google className="text-white hover:text-teal-300 cursor-pointer" />
+          </div>
+
+          <div className="flex flex-col items-center justify-center mt-4">
+            <input
+              id="registerName"
+              name="registerName"
+              type="text"
+              className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black"
+              placeholder="Name"
+              value={registerName}
+              onChange={(e) => setRegisterName(e.target.value)}
+            />
+            <input
+              id="registerEmail"
+              name="registerEmail"
+              type="email"
+              className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black"
+              placeholder="Email"
+              value={registerEmail}
+              onChange={(e) => setRegisterEmail(e.target.value)}
+            />
+            <input
+              id="registerPassword"
+              name="registerPassword"
+              type="password"
+              className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black"
+              placeholder="Password"
+              value={registerPassword}
+              onChange={(e) => setRegisterPassword(e.target.value)}
+            />
+            <input
+              id="registerInstitution"
+              name="registerInstitution"
+              type="text"
+              className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black"
+              placeholder="Institution"
+              value={registerInstitution}
+              onChange={(e) => setRegisterInstitution(e.target.value)}
+            />
+            <input
+              id="registerPlace"
+              name="registerPlace"
+              type="text"
+              className="inline rounded-2xl px-4 py-2 w-1/2 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black"
+              placeholder="Place"
+              value={registerPlace}
+              onChange={(e) => setRegisterPlace(e.target.value)}
+            />
+            <button
+              onClick={handleSignUp}
+              className="rounded-2xl m-2 text-teal-500 bg-white w-1/2 px-4 py-2 shadow-md hover:bg-teal-500 hover:text-white border-[2px] border-teal-500 transition duration-300 inline"
+            >
+              Sign Up
+            </button>
+          </div>
+
+          <p className="text-white m-4 text-sm">
+            Already have an account?
+            <Link
+              to="/login"
+              className="underline underline-offset-4 text-teal-300 ml-1 cursor-pointer hover:text-teal-400 transition duration-300"
+              onClick={() => setIsLogin(true)}
+            >
+              Sign In
+            </Link>
+          </p>
         </div>
-
-        <div className="flex flex-col items-center justify-center mt-4">
-          <input type="text" className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black" placeholder="Name" />
-          <input type="email" className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black" placeholder="Email" />
-          <input type="password" className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black" placeholder="Password" />
-          <input type="text" className="rounded-2xl px-4 py-2 w-4/5 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black" placeholder="Institution" />
-          <input type="text" className="inline rounded-2xl px-4 py-2 w-1/2 md:w-full border-[2px] border-teal-400 m-2 focus:shadow-lg focus:border-white focus:outline-none transition duration-300 text-black" placeholder="Place" />
-          <button className="rounded-2xl m-2 text-teal-500 bg-white w-1/2 px-4 py-2 shadow-md hover:bg-teal-500 hover:text-white border-[2px] border-teal-500 transition duration-300 inline">
-            Sign Up
-          </button>
-        </div>
-
-        <p className="text-white m-4 text-sm">
-          Already have an account?
-          <Link to='/login' className="underline underline-offset-4 text-teal-300 ml-1 cursor-pointer hover:text-teal-400 transition duration-300" onClick={() => setIsLogin(true)}>Sign In</Link>
-        </p>
-      </div>
-    )
-
-
-
-       {/* past end here */}
-
       </main>
     </div>
   );
